520571d0a05cabb9aa13a2791d413087dab125a2
aleung2 <35710340+aleung2@users.noreply.github.com>
2018-09-22 18:23:01 -0700

    Create responseCallData.java

---
+++ responseCallData.java
@@ -0,0 +1,15 @@
+public class responseCallData {
+	private int responseCall;
+	private String service;
+	private String location;
+	private String coordinates;
+	
+	public responseCallData(int responseCall, String service, String location,
+			String coordinates) {
+		this.responseCall = responseCall;
+		this.service = service;
+		this.location = location;
+		this.coordinates = coordinates;
+	}
+	
+}
